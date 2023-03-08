cfffe11d8107a64c17639901ec152f3a4201a573
matski <allaniwanowtrouiller2000@gmail.com>
2023-03-08 17:41:26 +0100

    TD1 Prog Odoo

---
+++ odoo/local-src/students/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Gestion des étudiants",
+    "version": "0.5",
+    "category": "Generic Modules/Others",
+    "description": """Test création module gestion des étudiants Odoo v14""",
+    "author": "Iwanow",
+    "data": ["views/students_views.xml","views/training_views.xml","views/menu_views.xml"],
+    "depends": ["base"],
+    "installable": True,
+    "auto_install": False,
+}
